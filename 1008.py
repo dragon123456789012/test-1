f40f837f3b79b4d5dbb4367109e1b637376e1350
바지회장 <talksis@nate.com>
2023-04-29 14:30:36 +0900

    first commit

---
+++ 1008.py
@@ -0,0 +1,6 @@
+#input -> 한줄을 입력 받는다!(통째로)
+#split -> 공백을 기준으로 나눈다.
+a, b = input().split()
+a = int(a) #int는 integer(정수)를 의미한다.
+b = int(b)
+print(a/b)
---
+++ 1330.py
@@ -0,0 +1,15 @@
+# 입력부
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+
+if a > b:  # if 조건 :
+    print(">")
+
+if a < b:
+    print("<")
+
+if a == b:
+    print("==")
